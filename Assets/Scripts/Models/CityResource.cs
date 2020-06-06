c04df3883d1977c74d2342721b993d0b6eba543b
yasshi2525 <yasshy2@gmail.com>
2020-06-06 17:46:20 +0900

    会社の生成処理を追加

---
+++ Assets/Scripts/Models/CityResource.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AddressableAssets;
+
+public class CityResource : MonoBehaviour
+{
+  public List<Company> cs;
+  public Company c;
+
+  private void Awake()
+  {
+    cs = new List<Company>();
+  }
+
+  // Start is called before the first frame update
+  private void Start()
+  {
+    var newC = Instantiate(c);
+    newC.transform.position = new Vector3(0.5f, -0.5f, 0.0f);
+    cs.Add(newC);
+  }
+
+  // Update is called once per frame
+  private void Update()
+  {
+
+  }
+}
---
+++ Assets/Scripts/Models/Company.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine.AddressableAssets;
+
+public class Company : MonoBehaviour
+{
+
+  private void Start()
+  {
+
+  }
+
+  void Update()
+  {
+
+  }
+}

0bb9e8b342a9c4c1adac89dd02dce23f26ed0c9f
yasshi2525 <yasshy2@gmail.com>
2020-06-11 18:24:06 +0900

    RailNodeのテストを追加

---
+++ Assets/Tests/RailNodeTest.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+  public class RailNodeTest
+  {
+    private TestUtils utils;
+
+    [SetUp]
+    public void Init()
+    {
+      utils = new TestUtils();
+    }
+
+    [UnityTest]
+    public IEnumerator CreateNewInstance()
+    {
+      yield return null;
+      var inst = utils.rn.NewInstance(new Vector3(1f, 2f, 3f));
+      yield return null;
+      Assert.AreEqual(utils.storage.Find<RailNode>().Count, 1);
+      Assert.AreEqual(inst.transform.position, new Vector3(1f, 2f, 3f));
+    }
+
+    [UnityTest]
+    public IEnumerator ExtendRail()
+    {
+      yield return null;
+      var inst = utils.rn.NewInstance(new Vector3(1f, 2f, 3f));
+      yield return null;
+      var outE = inst.Extend(new Vector3(4f, 5f, 6f));
+      yield return null;
+      Assert.AreEqual(utils.storage.Find<RailNode>().Count, 2);
+      Assert.AreEqual(inst.inEdge.Count, 1);
+      Assert.AreEqual(inst.outEdge.Count, 1);
+      Assert.AreEqual(inst.outEdge[0].from, inst);
+      Assert.AreEqual(inst.inEdge[0].to, inst);
+      Assert.AreEqual(inst.outEdge[0], outE);
+      Assert.AreEqual(inst.inEdge[0], outE.reverse);
+      Assert.AreEqual(inst.outEdge[0].from.transform.position, new Vector3(1f, 2f, 3f));
+      Assert.AreEqual(inst.outEdge[0].to.transform.position, new Vector3(4f, 5f, 6f));
+      Assert.AreEqual(inst.inEdge[0].from.transform.position, new Vector3(4f, 5f, 6f));
+      Assert.AreEqual(inst.inEdge[0].to.transform.position, new Vector3(1f, 2f, 3f));
+    }
+
+    [UnityTest]
+    public IEnumerator BuildPlatform()
+    {
+      yield return null;
+      var inst = utils.rn.NewInstance(new Vector3(1f, 2f, 3f));
+      var p = inst.BuildStation();
+      yield return null;
+      Assert.AreEqual(inst.platform, p);
+      Assert.AreEqual(p.on, inst);
+    }
+  }
+}
---
+++ Assets/Tests/TestUtils.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class TestUtils
+{
+  public ModelStorage storage;
+  public ModelListener listener;
+  public ModelFactory factory;
+  public Company c;
+  public Residence r;
+  public RailNode rn;
+  public RailEdge re;
+  public RailPart rp;
+  public Station st;
+  public Platform p;
+  public Gate g;
+  public Train t;
+  public Human h;
+  public TestUtils()
+  {
+    storage = new GameObject().AddComponent<ModelStorage>();
+    listener = new GameObject().AddComponent<ModelListener>();
+    factory = new GameObject().AddComponent<ModelFactory>();
+
+    var co = new GameObject();
+    co.AddComponent<SpriteRenderer>();
+    factory.c = c = co.AddComponent<Company>();
+
+    var ro = new GameObject();
+    ro.AddComponent<SpriteRenderer>();
+    factory.r = r = ro.AddComponent<Residence>();
+
+    factory.rn = rn = new GameObject().AddComponent<RailNode>();
+
+    factory.re = re = new GameObject().AddComponent<RailEdge>();
+
+    var rpo = new GameObject();
+    rpo.AddComponent<MeshRenderer>();
+    factory.rp = rp = rpo.AddComponent<RailPart>();
+
+    var sto = new GameObject();
+    sto.AddComponent<SpriteRenderer>();
+    factory.st = st = sto.AddComponent<Station>();
+
+    factory.p = p = new GameObject().AddComponent<Platform>();
+
+    factory.g = g = new GameObject().AddComponent<Gate>();
+
+    var to = new GameObject();
+    to.AddComponent<SpriteRenderer>();
+    factory.t = t = to.AddComponent<Train>();
+
+    var ho = new GameObject();
+    ho.AddComponent<SpriteRenderer>();
+    factory.h = h = ho.AddComponent<Human>();
+
+    c.listener = r.listener = rn.listener = re.listener = rp.listener = st.listener = p.listener = g.listener = t.listener = h.listener = listener;
+    c.storage = r.storage = rn.storage = re.storage = rp.storage = st.storage = p.storage = g.storage = t.storage = h.storage = storage;
+    r.factory = rn.factory = re.factory = st.factory = factory;
+  }
+}
